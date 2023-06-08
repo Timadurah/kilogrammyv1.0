6a45bd09139d4234e59956b37a5daf114fce0a12
tim.adurah <117412844+kilogrammie@users.noreply.github.com>
2023-06-08 18:56:46 +0100

    Add files via upload

---
+++ meta-tag/index.php
@@ -0,0 +1,85 @@
+<?php
+// ++++Import the task to make some work easier+++++
+require '../workers/task.php'
+    ?>
+
+
+<!-- Html code goes here -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+     <link rel="shortcut icon" href="../images/Google-Ranking-through-SEO.png" type="image/x-icon">
+     <title>Meta tag checker -Kilogrammy</title>
+           
+<meta property="og:site_name" content="Kilogrammy" />
+
+<meta property="og:type" content="website" />
+
+<meta property="og:url" content="https://www.kilogrammy.com/seo" />
+
+<meta name="twitter:card" content="summary_large_image" />
+
+<meta name="twitter:creator" content="@The_kilogrammy" />
+
+<meta name="twitter:site" content="@The_kilogrammy" />
+
+<meta property="og:title" content="Kilogrammy -Check your website to know how slack it is to seo world" />
+
+<meta name="twitter:title" content="Kilogrammy -Check your website to know how slack it is to seo world" />
+
+<meta
+  property="og:description"
+  content="Drive more traffic and increase your online visibility with the help of our SEO company. Our team of experts uses the latest SEO techniques to optimize your website and help it rank higher in search engine results. With our proven track record of success, you can trust us to boost your website's ranking, attract more qualified leads, and increase your online revenue. Whether you need a one-time optimization or ongoing SEO support, we have the skills and expertise to deliver results. Contact us today to start your SEO journey."
+/>
+
+<meta
+  name="description"
+  content="Drive more traffic and increase your online visibility with the help of our SEO company. Our team of experts uses the latest SEO techniques to optimize your website and help it rank higher in search engine results. With our proven track record of success, you can trust us to boost your website's ranking, attract more qualified leads, and increase your online revenue. Whether you need a one-time optimization or ongoing SEO support, we have the skills and expertise to deliver results. Contact us today to start your SEO journey."
+/>
+
+<meta
+  property="og:image"
+  content="../images/Google-Ranking-through-SEO.png"
+/>
+
+<meta
+  name="twitter:image"
+  content="../images/Google-Ranking-through-SEO.png"
+/> 
+   <link rel="stylesheet" href="../style/kilogrammy.css">
+     <!-- bootrap cdn -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.min.css"
+          integrity="sha512-Ez0cGzNzHR1tYAv56860NLspgUGuQw16GiOOp/I2LuTmpSK9xDXlgJz3XN4cnpXWDmkNBKXR/VDMTCnAaEooxA=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <!-- bootrap icons -->
+     <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css"
+          integrity="sha512-YFENbnqHbCRmJt5d+9lHimyEMt8LKSNTMLSaHjvsclnZGICeY/0KYEeiHwD1Ux4Tcao0h60tdcMv+0GljvWyHg=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+</head>
+<body>
+    
+
+    <?php
+    // Import navigation Cpnts
+    Import('../components/nav.php');
+
+    // Import navigation Cpnts
+    Import('../components/meta.php');
+
+    // Import footer Cpnts
+    Import('../components/footer.php');
+    ?>
+
+
+</body>
+
+</html>
---
+++ mobile-development-company-in-nigeria-lagos/index.php
@@ -0,0 +1,84 @@
+<?php
+// ++++Import the task to make some work easier+++++
+require '../workers/task.php'
+    ?>
+
+
+<!-- Html code goes here -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1">
+     <link rel="shortcut icon" href="../images/banner_mobile.png" type="image/x-icon">
+     <title>Best Mobile App development Company in Nigeria Lagos</title>
+         
+<meta property="og:site_name" content="Kilogrammy" />
+
+<meta property="og:type" content="website" />
+
+<meta property="og:url" content="https://www.kilogrammy.com/mobile-development-company-in-nigeria-lagos" />
+
+<meta name="twitter:card" content="summary_large_image" />
+
+<meta name="twitter:creator" content="@The_kilogrammy" />
+
+<meta name="twitter:site" content="@The_kilogrammy" />
+
+<meta property="og:title" content="Kilogrammy -Best Mobile App Development company in Nigeria Lagos" />
+
+<meta name="twitter:title" content="Kilogrammy -Best Mobile App Development company in Nigeria Lagos" />
+
+<meta
+  property="og:description"
+  content="Transform your business with a cutting-edge mobile app developed by our mobile app development company. Our team of expert developers and designers create custom, user-friendly mobile apps that deliver exceptional user experiences. Whether you need a simple app for your business or a complex app with multiple features, we have the skills and expertise to bring your vision to life. Stay ahead of the competition and engage with your customers on-the-go. Contact us today to start your app development journey.."
+/>
+
+<meta
+  name="description"
+  content="Transform your business with a cutting-edge mobile app developed by our mobile app development company. Our team of expert developers and designers create custom, user-friendly mobile apps that deliver exceptional user experiences. Whether you need a simple app for your business or a complex app with multiple features, we have the skills and expertise to bring your vision to life. Stay ahead of the competition and engage with your customers on-the-go. Contact us today to start your app development journey.."
+/>
+
+<meta
+  property="og:image"
+  content="../images/banner_mobile.png"
+/>
+
+<meta
+  name="twitter:image"
+  content="../images/banner_mobile.png"
+/>  <link rel="stylesheet" href="../style/kilogrammy.css">
+     <!-- bootrap cdn -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.1/css/bootstrap.min.css"
+          integrity="sha512-Ez0cGzNzHR1tYAv56860NLspgUGuQw16GiOOp/I2LuTmpSK9xDXlgJz3XN4cnpXWDmkNBKXR/VDMTCnAaEooxA=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <!-- bootrap icons -->
+     <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css"
+          integrity="sha512-YFENbnqHbCRmJt5d+9lHimyEMt8LKSNTMLSaHjvsclnZGICeY/0KYEeiHwD1Ux4Tcao0h60tdcMv+0GljvWyHg=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+
+</head>
+<body>
+    
+
+    <?php
+    // Import navigation Cpnts
+    Import('../components/nav.php');
+
+    // Import navigation Cpnts
+    Import('../components/mobile.php');
+
+    // Import footer Cpnts
+    Import('../components/footer.php');
+    ?>
+
+
+</body>
+
+</html>
